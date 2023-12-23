39a379ed08fa4d59f47f60ce5d5187667823cd04
Sfa11eN <sfallenn@gmail.com>
2023-12-23 19:48:08 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,19 @@ public class Main {
         System.out.println("Задание 1");
 
 
-        for (int i = 1; i <= 10; i++)
+        for (int i = 1; i <= 10; i++) {
             System.out.println(i);
+        }
 
         System.out.println(" Задание 2 ");
 
         for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println(" Задание 3 ");
+
+        for (int i = 0; i <= 17; i+=2) {
             System.out.println(i);
         }
 
