4471ac54f2f023fda9539e4887a5440e933033f2
Sfa11eN <sfallenn@gmail.com>
2023-12-23 18:16:39 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Циклы");
+
+        System.out.println("Задание 1");
+
+
+        for (int i = 1; i <= 10; i++)
+            System.out.println(i);
+
+        System.out.println(" Задание 2 ");
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+
+    }
+}
