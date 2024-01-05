1ce2bc5e582226fee959428a70013b83eef63507
Sfa11eN <sfallenn@gmail.com>
2024-01-05 16:45:57 +0300

    Just  commit

--- src/Main.java
+++ src/Main.java
@@ -21,14 +21,36 @@ public class Main {
 
         System.out.println(" Задание 3 ");
 
-        for (int i = 0; i <= 17; i+=2) {
+        for (int i = 0; i <= 17; i += 2) {
             System.out.println(i);
         }
 
         System.out.println(" Задание 4 ");
 
+        for (int i = 10; i >= -10; i -= 1) {
+            System.out.println(i);
+        }
+        System.out.println(" Задание 5 ");
 
+        for (int year = 1904; year <= 2096; year += 4) {
+            System.out.println(year + " год является високосным");
+        }
+        System.out.println(" Задание 6 ");
 
+        for (int i = 7; i <= 98; i += 7) {
+            System.out.println(i + " ");
+        }
+        System.out.println(" Задание 7 ");
 
+        for (int i = 1; i <= 512; i *= 2) {
+            System.out.println(i + " ");
+        }
+        System.out.println(" Задание 8 ");
+
+        int deferredAmount = 29000;
+
+        for (int amount = 1; amount <= 12; amount++) {
+            System.out.println(" Месяц " + amount + " сумма накоплений " + amount * deferredAmount + " рублей ");
+        }
     }
 }
